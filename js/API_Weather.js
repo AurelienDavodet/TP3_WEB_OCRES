8da6520d2f916a0f1da17f26e576c241e2dd273d
AurelienDavodet <aurelien.davodet@gmail.com>
2020-10-12 22:55:22 +0200

    Questions4

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,15 +3,18 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_DAY_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_3DAY_URL = "http://api.openweathermap.org/data/2.5/forecast/daily";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
+
 
 var val = "rien";
 
 function controle() {
   val = document.getElementById("city-input").value;
   start();
+  getThreeDayForecast();
    
 }
 
@@ -20,21 +23,35 @@ class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
     if(val === "rien"){
-      city = "paris";
+      this.city = "Paris";
     }
-    this.city = val;
+    else {
+      this.city = val;
+    }
   }
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
   fetchTodayForecast(){
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+    .get(`${API_DAY_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
       crossdomain: true
     })
   }
+
+  fetchThreeDayForecast(){
+    return axios
+    .get(`${API_3DAY_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+  }
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
   }
+
+
 }
+
+
--- js/script.js
+++ js/script.js
@@ -30,5 +30,52 @@ function start() {
     });
 }
 
+function getThreeDayForecast(){
 
+  // Création de l'objet apiWeather
+  const apiWeather = new API_WEATHER();
+  // Appel de la fonction fetchTodayForecast
+  
+  apiWeather
+    .fetchThreeDayForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data = response.data;
+
+      //boucle qui permet d'afficher les 3 premiers jours
+      for(let i=1; i<4; i++)
+      {
+        // On récupère l'information principal
+        const main = data.list[i].weather[0].main;
+        const description = data.list[i].weather[0].description;
+        const temp = data.list[i].temp.day;
+        const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+
+        
+        if(i==1){
+          document.getElementById('demain-forecast-main').innerHTML = main;
+          document.getElementById('demain-forecast-more-info').innerHTML = description;
+          document.getElementById('icon2-weather-container').innerHTML = icon;
+          document.getElementById('demain-forecast-temp').innerHTML = `${temp}°C`;
+        }
+        else if(i==2){
+          document.getElementById('apdemain-forecast-main').innerHTML = main;
+          document.getElementById('apdemain-forecast-more-info').innerHTML = description;
+          document.getElementById('icon3-weather-container').innerHTML = icon;
+          document.getElementById('apdemain-forecast-temp').innerHTML = `${temp}°C`;
+        }
+        else {
+          document.getElementById('eapdemain-forecast-main').innerHTML = main;
+          document.getElementById('eapdemain-forecast-more-info').innerHTML = description;
+          document.getElementById('icon4-weather-container').innerHTML = icon;
+          document.getElementById('eapdemain-forecast-temp').innerHTML = `${temp}°C`;
+        }
+      }
+      
+    })
+    .catch(function(error) {
+      // Affiche une erreur
+      console.error(error);
+    });
+}
 
